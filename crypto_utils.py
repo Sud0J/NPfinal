f29c3d56be20d1e1afdae0527659f066736ef05f
Sud0J <quangnn.23bi14376@usth.edu.vn>
2025-05-05 22:26:12 +0700

    Create crypto_utils.py

---
+++ crypto_utils.py
@@ -0,0 +1,38 @@
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import os
+
+def generate_keys():
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+    public_key = private_key.public_key()
+    return private_key, public_key
+
+def encrypt_message(message, public_key):
+    return public_key.encrypt(
+        message.encode(),
+        padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                     algorithm=hashes.SHA256(), label=None)
+    )
+
+def decrypt_message(ciphertext, private_key):
+    return private_key.decrypt(
+        ciphertext,
+        padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                     algorithm=hashes.SHA256(), label=None)
+    ).decode()
+
+def generate_symmetric_key():
+    return os.urandom(32)  # AES-256 key
+
+def encrypt_data(data, key):
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv))
+    encryptor = cipher.encryptor()
+    return iv + encryptor.update(data) + encryptor.finalize()
+
+def decrypt_data(data, key):
+    iv = data[:16]
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv))
+    decryptor = cipher.decryptor()
+    return decryptor.update(data[16:]) + decryptor.finalize()
